b408f602f0db4fbaff9a6eae9daffcbb2aee023e
Ken Barber <ken@bob.sh>
2011-06-26 20:09:47 +0200

    Add capabilities for zone views.

--- manifests/site-solutions.pp
+++ manifests/site-solutions.pp
@@ -180,7 +180,25 @@ node /node\d+\.cloud\.*/ {
   class { "bind":
   }
 
-  bind::zone { "vms.cloud.bob.sh":
+  bind::view { "internal":
+    match_clients => ["192.168.1.0/24"],
+    options => {
+      'check-names' => { 'master' => 'fail' },
+    }
+  }
+
+  bind::view::zone { "internal:mydomain.com":
+    type => "master",
+    zone_contact => "ken@mydomain.com",
+    options => {
+      'allow-update' => ['127.0.0.1'],
+    }
+  }
+
+  bind::view { "external":
+  }
+
+  bind::view::zone { "external:vms.cloud.bob.sh":
     type => "master",
     zone_contact => "root@bob.sh",
     options => {
@@ -189,24 +207,9 @@ node /node\d+\.cloud\.*/ {
     },
   }
 
-  bind::zone { "2.1.10.in-addr.arpa":
+  bind::view::zone { "external:2.1.10.in-addr.arpa":
     type => "master",
     zone_contact => "root@bob.sh",
-    options => {
-      'allow-update' => ['127.0.0.1'],
-    }
-  }
-
-  bind::view { "internal":
-    match_clients => ["192.168.1.0/24"],
-    options => {
-      'check-names' => 'master',
-    }
-  }
-
-  bind::view::zone { "internal:mydomain.com":
-    type => "master",
-    zone_contact => "ken@mydomain.com",
     options => {
       'allow-update' => ['127.0.0.1'],
     }
