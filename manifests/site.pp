7f793b4177b96e9876b1ca8108a7b7dd7a702bce
Ken Barber <ken@bob.sh>
2011-09-26 19:12:26 +0200

    Update site.pp.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,39 +1,7 @@
-# Defaults
+import 'globals.pp'
 import 'solutions.pp'
-resources { "firewall": purge => true }
-filebucket { main: server => puppet }
-File { backup => main }
+import 'nodes.pp'
 
+file { "/tmp/foo": content => "barfoo" }
 
-# Stages
-stage { "pre": before => Stage["main"] }
-stage { "post": require => Stage["main"] }
-
-node /node\d+\.cloud\.*/ {
-  class { "my_aptmirror": }
-  class { "my_virtnode": }
-  class { "my_dns": }
-  class { "my_onecontroller": }
-  class { "my_mq": }
-  class { "my_puppet": master => true }
-
-#  app_stuck { "pastebin.bob.sh":
-#    db_servers => 1,
-#    web_servers => 2,
-#    lb_servers => 2,
-#  }
-  app_basetomcat { "tc.bob.sh":
-    db_servers => 1,
-    web_servers => 2,
-    lb_servers => 2,
-  }
-
-  class { "my_soe": }
-}
-
-node /^.+\.vms\.cloud\./ {
-  Export_classes <<| tag == "vm_${fqdn}" |>>
-
-  class { "my_soe": }
-}
-
+notice("$swapsize")
