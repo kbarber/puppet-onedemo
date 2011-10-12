f049cd37c770597113b21ef36ac4c5fe382c70a3
Ken Barber <ken@bob.sh>
2011-10-12 12:53:52 +0200

    Update nodes.pp to use app_stuck application.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,47 +6,47 @@ node /node\d+\.cloud\.*/ {
   class { "my_mq": }
   class { "my_puppet": master => true }
 
-#  app_stuck { "pastebin.bob.sh":
-#    db_servers => 1,
-#    web_servers => 2,
-#    lb_servers => 2,
-#  }
-  app_basetomcat { "tc.bob.sh":
-    db_servers => 0,
-    web_servers => 4,
-    lb_servers => 0,
+  app_stuck { "pastebin.bob.sh":
+    db_servers => 1,
+    web_servers => 2,
+    lb_servers => 2,
   }
+#  app_basetomcat { "tc.bob.sh":
+#    db_servers => 0,
+#    web_servers => 4,
+#    lb_servers => 0,
+#  }
 
 
   # virt1
-  $disks = [
-      { image => "deb-wheezy-amd64-2",
-        driver => "qcow2",
-        target => "vda" },{}
-  ]
-  $nics = [
-     { network => "internal",
-       model => "virtio" },{}
-  ]
-  onevm { "virt1.vms.cloud.bob.sh":
-    memory => "256",
-    cpu => 1,
-    vcpu => 1,
-    os_arch => "x86_64",
-    disks => $disks,
-    nics => $nics,
-    graphics_type => "vnc",
-    graphics_listen => "0.0.0.0",
-    context => {
-      hostname => '$NAME',
-      gateway => '$NETWORK[GATEWAY]',
-      dns => '$NETWORK[DNS]',
-      ip => '$NIC[IP]',
-      files => '/var/lib/one/context/init.sh',
-      target => "vdb",
-      puppet_environment => "onedemo",
-    }
-  }
+#  $disks = [
+#      { image => "deb-wheezy-amd64-2",
+#        driver => "qcow2",
+#        target => "vda" },{}
+#  ]
+#  $nics = [
+#     { network => "internal",
+#       model => "virtio" },{}
+#  ]
+#  onevm { "virt1.vms.cloud.bob.sh":
+#    memory => "256",
+#    cpu => 1,
+#    vcpu => 1,
+#    os_arch => "x86_64",
+#    disks => $disks,
+#    nics => $nics,
+#    graphics_type => "vnc",
+#    graphics_listen => "0.0.0.0",
+#    context => {
+#      hostname => '$NAME',
+#      gateway => '$NETWORK[GATEWAY]',
+#      dns => '$NETWORK[DNS]',
+#      ip => '$NIC[IP]',
+#      files => '/var/lib/one/context/init.sh',
+#      target => "vdb",
+#      puppet_environment => "onedemo",
+#    }
+#  }
 
   class { "my_soe": }
 
